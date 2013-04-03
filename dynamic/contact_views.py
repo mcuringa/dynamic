58a6df7faa457a5e9e6fcf00ff03e278033ee504
mxc <matt@curinga.com>
2013-04-03 12:38:20 -0400

    working on ContactForm, need to test and configure email/SMTP

---
+++ dynamic/contact_views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from dynamic.models import *
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
+from django.contrib.auth.forms import UserCreationForm
+
+from django.http import HttpResponse, HttpResponseRedirect
+
+from django.core.mail import send_mail, BadHeaderError
+
+from datetime import datetime
+
+def contact(request):
+    
+    admin_email = "matt@curinga.com"
+
+    if request.method == 'POST': # If the form has been submitted...
+        form = ContactForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            
+            # add a header to the subject, to make it easier to sort/find
+            # in email client
+            subject = "[Apps4Ed Contact] {}".format(subject)
+            
+            # add the sender's name and current time to the body
+            # of the email
+            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            message = """From: {}
+Time Received: {}
+Message:
+{}""".format(form.from_name, now, form.message)
+            
+            #try to send it
+            try:
+                send_mail(subject, message, form.from_email, [admin_email])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+
+            messages.add_message(request, messages.INFO, "Thank you for contacting us!")
+            return HttpResponseRedirect('/')
+
+        else:
+            form = ContactForm()
+
+    return render(request, 'contact.html', {'form': form,})
+
